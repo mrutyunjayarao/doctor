83fda49ab3421741178c3f0cfb0f26c2b4b36b60
roop-sagarh <talariroopsagar@gmail.com>
2022-11-23 18:29:03 +0530

    registered users page

--- src/app/components/registered-users/registered-users.component.ts
+++ src/app/components/registered-users/registered-users.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+export interface Data {
+  id: number;
+  name: string;
+  phone: number;
+  email: string;
+  status: string;
+  disabled: boolean;
+}
 @Component({
   selector: 'app-registered-users',
   templateUrl: './registered-users.component.html',
@@ -8,8 +15,64 @@ import { Component, OnInit } from '@angular/core';
 export class RegisteredUsersComponent implements OnInit {
 
   constructor() { }
+  checked = false;
+  loading = false;
+  indeterminate = false;
+  listOfData: readonly Data[] = [];
+  listOfCurrentPageData: readonly Data[] = [];
+  setOfCheckedId = new Set<number>();
 
-  ngOnInit(): void {
+  updateCheckedSet(id: number, checked: boolean): void {
+    if (checked) {
+      this.setOfCheckedId.add(id);
+    } else {
+      this.setOfCheckedId.delete(id);
+    }
   }
 
-}
+  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
+    this.listOfCurrentPageData = listOfCurrentPageData;
+    this.refreshCheckedStatus();
+  }
+
+  refreshCheckedStatus(): void {
+    const listOfEnabledData = this.listOfCurrentPageData.filter(({ disabled }) => !disabled);
+    this.checked = listOfEnabledData.every(({ id }) => this.setOfCheckedId.has(id));
+    this.indeterminate = listOfEnabledData.some(({ id }) => this.setOfCheckedId.has(id)) && !this.checked;
+  }
+
+  onItemChecked(id: number, checked: boolean): void {
+    this.updateCheckedSet(id, checked);
+    this.refreshCheckedStatus();
+  }
+
+  onAllChecked(checked: boolean): void {
+    this.listOfCurrentPageData
+      .filter(({ disabled }) => !disabled)
+      .forEach(({ id }) => this.updateCheckedSet(id, checked));
+    this.refreshCheckedStatus();
+  }
+
+  sendRequest(): void {
+    this.loading = true;
+    const requestData = this.listOfData.filter(data => this.setOfCheckedId.has(data['id']));
+    console.log(requestData);
+    setTimeout(() => {
+      this.setOfCheckedId.clear();
+      this.refreshCheckedStatus();
+      this.loading = false;
+    }, 1000);
+  }
+
+  ngOnInit(): void {
+    this.listOfData = new Array(5).fill(0).map((_, index) => ({
+      id: index,
+      name: `Edward King ${index}`,
+      phone: 9823432323+index+1,
+      email: `edward${index}@gmail.com`,
+      status: 'good',
+      disabled: false
+    }));
+  }
+  }
+
